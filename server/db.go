3c94c28ca83e83f79c7f7aa3a7f15059b7b8e058
liujunren <768712894@QQ.COM>
2021-07-07 11:36:33 +0800

    dev

--- server/db.go
+++ server/db.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/liujunren93/admin/global"
 	"os"
+	"path/filepath"
 )
 //go:embed static/db.file
 var dbStr string
@@ -12,16 +13,17 @@ var dbStr string
 
 
 func NewDBFile() {
-	err := os.MkdirAll(global.FilePath[global.TypeDb], 0777)
+	path, err := filepath.Abs(global.ApiRoot + "/" + global.FilePath[global.TypeDb])
+	err = os.MkdirAll(path, 0777)
 	if err != nil {
 		panic(err)
 	}
-	create, err := os.Create(global.FilePath[global.TypeDb] + "/mysql.go")
+	create, err := os.Create(path+ "/mysql.go")
 	if err != nil {
 		panic(err)
 	}
 	defer create.Close()
 	dsn :=`fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", mysqlConf.User, mysqlConf.Password, mysqlConf.Host, mysqlConf.Port, mysqlConf.DB)`
-	create.WriteString(fmt.Sprintf(dbStr, global.FilePath[global.TypeDb],dsn))
+	create.WriteString(fmt.Sprintf(dbStr,global.Mod+"/conf",dsn))
 
 }
